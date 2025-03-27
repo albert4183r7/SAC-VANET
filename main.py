be428540d480b59964fc9868e76c99a3c052128a
Albert <88728615+albert4183r7@users.noreply.github.com>
2025-03-27 20:54:22 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparse
+from train_and_evaluate import train_sac_on_environment, evaluate_sac_model
+import subprocess
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", action="store_true", help="Train the SAC model")
+    parser.add_argument("--eval", action="store_true", help="Evaluate the SAC model")
+    parser.add_argument("--serve", action="store_true", help="Run inference socket server")
+    args = parser.parse_args()
+
+    if args.train:
+        train_sac_on_environment()
+    if args.eval:
+        evaluate_sac_model()
+    if args.serve:
+        subprocess.run(["python", "inference_server.py"])
